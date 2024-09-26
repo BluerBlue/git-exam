6d3eb214eaf46de1ef636587d76d04df309e5722
sto <yuvin3493@gmail.com>
2024-09-26 11:10:23 +0900

    modify test03.py by user01

--- test03.py
+++ test03.py
@@ -1,8 +1,10 @@
 #test03.py
 
 def sayHello(name):
-    return 'Hello, ' + name
+    return 'Hello, ' + name + '!!' #user01 수정
     
 inName = Tom
 
 print(sayHello(inName))
+
+print('Requested by boss') #user01 추가
